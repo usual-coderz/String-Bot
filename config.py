6e66290601fceaa223fee1835192c68e3632b688
usual-coderz <nexacoders2@gmail.com>
2026-02-25 17:05:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,16 @@ MONGO_URL = getenv("MONGO_URL")
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VeronMeetup")
 
+
+# =====================================
+# FORCE JOIN (DEFAULT CHANNELS)
+# =====================================
+
+FORCE_JOIN = [
+    "https://t.me/VeronUpdates"
+]
+
+
 # Add these lines
 API_ID = int(getenv("API_ID", 0))           # Telegram API ID
 API_HASH = getenv("API_HASH", "")          # Telegram API Hash
