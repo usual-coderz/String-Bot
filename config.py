42c1767018b02a9908e6ec2358983df0698c6a0b
usual-coderz <nexacoders2@gmail.com>
2026-02-25 16:09:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ load_dotenv()
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_URL = getenv("MONGO_URL")
-
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VeronCoders")
+
+# Add these lines
+API_ID = int(getenv("API_ID", 0))           # Telegram API ID
+API_HASH = getenv("API_HASH", "")          # Telegram API Hash
