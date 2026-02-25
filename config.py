3fd18661b9934eb1e79f0d205c5737cbd5817e38
usual-coderz <nexacoders2@gmail.com>
2026-02-25 16:02:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_URL = getenv("MONGO_URL")
 
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VeronCoders")
